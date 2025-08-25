762704408e4bdfd04af3c296642bb1b9d4314ccb
Shuhib <shuhib_s@live.de>
2025-08-25 12:16:19 +0200

    setup init

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,5 +1,17 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
 
+import qwikdev from '@qwikdev/astro';
+
+import alpinejs from '@astrojs/alpinejs';
+import tailwindcss from '@tailwindcss/vite';
+import db from '@astrojs/db';
+
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+  integrations: [qwikdev(), alpinejs(), db()],
+
+  vite: {
+    plugins: [tailwindcss()]
+  }
+});
