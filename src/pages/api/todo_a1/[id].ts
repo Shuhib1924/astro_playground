6e604ee79455894a91800d56db90a93e802769ad
Shuhib <shuhib_s@live.de>
2025-08-26 08:28:23 +0200

    todo_a1: GET single

--- src/pages/api/todo_a1/[id].ts
+++ src/pages/api/todo_a1/[id].ts
@@ -4,13 +4,23 @@ export const prerender = false;
 // export const getStaticPaths = () => {
 //     return db.select().from(Todo_a1).then((todos) => todos.map((todo) => ({ params: { id: todo.id.toString() } })));
 // }
-export const GET: APIRoute = async ({ request, params }) => {
-    // const [todo] = await db.select().from(Todo_a1).where(eq(Todo_a1.id, Number(params.id)));
-    const id = params.id ? parseInt(params.id, 10) : NaN;
-    const [todo] = await db.select().from(Todo_a1).where(eq(Todo_a1.id, id));
-    console.log("todo ", todo);
-    if (!todo) {
-        return new Response('Todo not found', { status: 404 });
+// export const GET: APIRoute = async ({ request, params }) => {
+//     // const [todo] = await db.select().from(Todo_a1).where(eq(Todo_a1.id, Number(params.id)));
+//     const id = params.id ? parseInt(params.id, 10) : NaN;
+//     const [todo] = await db.select().from(Todo_a1).where(eq(Todo_a1.id, id));
+//     console.log("todo ", todo);
+//     if (!todo) {
+//         return new Response('Todo not found', { status: 404 });
+//     }
+//     return new Response(JSON.stringify(todo), { status: 200, headers: { 'Content-Type': 'application/json' } });
+// }
+
+export const GET: APIRoute = async ({ params }) => {
+    if (!params?.id) {
+        return new Response(JSON.stringify({ error: 'Missing id parameter' }), { status: 400 });
     }
-    return new Response(JSON.stringify(todo), { status: 200, headers: { 'Content-Type': 'application/json' } });
-}
+    // const id = parseInt(params.id, 10);
+    const id = parseInt(params.id)?? 0;
+    const [todo] = await db.select().from(Todo_a1).where(eq(Todo_a1.id, id));
+    return new Response(JSON.stringify(todo));
+} 
